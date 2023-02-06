2fa303e34d9c2a5eb645a576b13a66046f844ea1
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-02-06 15:08:47 -0300

    Requisições PUT e DELETE - Requisições DELETE

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -35,4 +35,10 @@ public class MedicoController {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
 }
