b03ac32b15c118f523dea522318e11ec5622f860
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-06-06 19:53:31 -0300

    Implementando o ResponseEntity no Crud de pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,12 +20,14 @@ public class PacienteController {
     private PacienteRepository pacienteRepository;
     @PostMapping
     @Transactional
-    public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dadosCadastroPaciente){
+    public ResponseEntity cadastrarPaciente(@RequestBody DadosCadastroPaciente dadosCadastroPaciente){
         pacienteRepository.save(new Paciente(dadosCadastroPaciente));
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
-        return pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
+    public ResponseEntity<Page<DadosListagemPaciente>> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+        var paciente = pacienteRepository.findAll(paginacao).map(DadosListagemPaciente::new);
+        return ResponseEntity.ok().body(paciente);
     }
 }
