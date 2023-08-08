83f870ea43ff71b11a233b954fd4bc58e2d90d93
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-08-08 13:58:30 -0300

    Validador para verificar se o paciente já possui consulta na data informada

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -2,9 +2,12 @@ package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
 
     boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
