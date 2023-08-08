df073bc7aaaf5351c65185b76ece7eb3b3b2b61c
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-08-08 12:22:37 -0300

    Validador médico ativo

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -23,4 +23,11 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
         limit 1
         """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+
+
+    @Query("""
+            select m.ativo from Medico m where m.id = :id
+            """)
+    Boolean findAtivoById(Long idMedico);
 }
