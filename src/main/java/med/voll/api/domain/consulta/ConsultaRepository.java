6c9ac3ae7a1650aaeabd46c31e95858b4f5c28e7
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-08-08 12:28:13 -0300

    Criando validador para verificar se o médico ja possui uma consulta agendada no mesmo dia e hora

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -2,5 +2,9 @@ package med.voll.api.domain.consulta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
