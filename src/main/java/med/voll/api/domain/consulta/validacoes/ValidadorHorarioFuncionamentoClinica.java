fd52825bbc9d31bb856c435a0f2488b4ea957865
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-08-07 22:38:30 -0300

    Verificando se o agendamento da consulta é no domingo e lançando exceção

---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -0,0 +1,17 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+
+import java.time.DayOfWeek;
+
+public class ValidadorHorarioFuncionamentoClinica {
+    public void validar(DadosAgendamentoConsulta dados){
+        var dataConsulta = dados.data();
+
+        var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+        if (domingo){
+            throw new ValidacaoException("A clínica não abre aos domingos.");
+        }
+    }
+}
