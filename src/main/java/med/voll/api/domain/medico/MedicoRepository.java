d319f50f1bcac1e3be22641a2af1a6dfa6da91d7
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-08-09 23:43:47 -0300

    Corrigindo o nome do parametro na interface medico repository

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -29,5 +29,5 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     @Query("""
             select m.ativo from Medico m where m.id = :id
             """)
-    Boolean findAtivoById(Long idMedico);
+    Boolean findAtivoById(Long id);
 }
