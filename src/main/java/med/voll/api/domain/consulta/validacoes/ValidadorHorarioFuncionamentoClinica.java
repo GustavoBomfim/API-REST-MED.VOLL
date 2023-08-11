06e869115d5a313536d58b3d182c83221c6c1129
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2023-08-10 21:35:58 -0300

    Corrigindo validação de horário de funcionamento da clínica

--- src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorHorarioFuncionamentoClinica.java
@@ -12,7 +12,7 @@ public class ValidadorHorarioFuncionamentoClinica implements ValidadorAgendament
         var dataConsulta = dados.data();
 
         var domingo = dataConsulta.getDayOfWeek().equals(DayOfWeek.SUNDAY);
-        var antesAberturaDaClinica = dataConsulta.getHour() > 7;
+        var antesAberturaDaClinica = dataConsulta.getHour() < 7;
         var depoisAberturaDaClinica = dataConsulta.getHour() > 18;
         if (domingo || antesAberturaDaClinica || depoisAberturaDaClinica){
             throw new ValidacaoException("Consulta fora do horário de funcionamento da clínica.");
